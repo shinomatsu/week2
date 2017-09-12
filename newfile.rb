27f0dcda722ee4f2b1e58babf2a8c2226719fdc3
shinomatsu <shi968k5@gmail.com>
2017-09-12 23:09:53 +0800

    newfileを更新

--- newfile.rb
+++ newfile.rb
@@ -1,1 +1,25 @@
 newfile
+# Example 1:
+
+user1 = { name: "Tom", gender: "M" }
+user2 = { name: "Ping", age: "21" }
+user3 = { name: "Ming Xiang", language: "Elixir"  }
+
+# 1. For each hash, what are the keys and values?
+
+# user1: 
+# key: name, value: Tom
+# key: gender, value: "M"
+
+# user2:
+
+
+# user3:
+
+
+# 2. How can we access the value "Tom"?
+p user1[:name]
+
+# 3. How can we access the value 21?
+
+# 4. How can we access the value "Elixir"?
